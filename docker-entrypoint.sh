b31e05d14faac27b3e280e9d82c337517f5b56b9
Symfonyaddict <formationsymfony5@gmail.com>
2026-06-28 11:35:26 +0200

    Fix : Ajout de importmap:install pour les assets

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,9 @@ echo "🧹 Préparation du cache..."
 php bin/console cache:clear --env=prod --no-warmup
 php bin/console cache:warmup --env=prod
 
+echo "📦 Installation des assets vendor (ImportMap)..."
+php bin/console importmap:install --env=prod --no-interaction
+
 echo "🎨 Compilation des assets..."
 php bin/console asset-map:compile --env=prod --no-interaction
 
